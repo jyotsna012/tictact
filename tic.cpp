d38b401f6f31e3ea364f2f6a22df501d58d7f219
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2021-10-27 15:48:19 -0700

    Update tic.cpp

--- tic.cpp
+++ tic.cpp
@@ -31,36 +31,32 @@ int main(){
  cin >> y;
  array[x-1][y-1] = 'X';
  cout << array[x-1][y-1] << endl;
- printBoard(array);
-  
+ for(int i = 0; i < 3; i++){
+   for(int j = 0; j < 3; j++){
+     cout << " " << array[i][j] << " ";
+   }
+   cout << endl;
+ }
+ cout << "-----------------------" << endl;  
 };
 
 void reset(char array[3][3]){
-
   for(int i = 0; i < 3; i++){
     for(int j = 0; j < 3; j++){
         array[i][j] = '_';
     }
   }
-  
   printBoard(array);
-   
   return;
-
 }
 
 void printBoard(char array[3][3]){
-
   for(int i = 0; i < 3; i++){
     for(int j = 0; j < 3; j++){
-
       cout << " " << array[i][j] << " ";
     }
     cout << endl;
   }
-  
   cout << "-----------------------" << endl;
-
-
   return;
 }
