1b690cdb17c2756b29d868ecb259a83d0b67d6bb
jyotsna012 <405456@bsd48.org>
2021-10-27 11:56:00 -0700

    hi

--- tic.cpp
+++ tic.cpp
@@ -1,2 +1,21 @@
-hellohi
-hi
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <cstring>
+
+using namespace std;
+
+int main(){
+
+int array[3][3] = {
+
+  {0,1,2},
+  {3,4,5},
+  {6,7,8}
+  
+};
+
+int a = array[0][1];
+
+cout << a << endl;
+};
