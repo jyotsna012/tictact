afbec80d714874c7e047728a102d9d9ce78e00af
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2021-10-27 16:56:06 -0700

    Update tic.cpp

--- tic.cpp
+++ tic.cpp
@@ -16,7 +16,7 @@ void Draw()
 	{
 		for (int j = 0; j < 3; j++)
 		{
-			cout << matrix[i][j] << " ";
+			cout << array[i][j] << " ";
 		}
 		cout << endl;
 	}
@@ -28,23 +28,23 @@ void Input()
 	cin >> a;
 
 	if (a == 1)
-		matrix[0][0] = player;
+		array[0][0] = player;
 	else if (a == 2)
-		matrix[0][1] = player;
+		array[0][1] = player;
 	else if (a == 3)
-		matrix[0][2] = player;
+		array[0][2] = player;
 	else if (a == 4)
-		matrix[1][0] = player;
+		array[1][0] = player;
 	else if (a == 5)
-		matrix[1][1] = player;
+		array[1][1] = player;
 	else if (a == 6)
-		matrix[1][2] = player;
+		array[1][2] = player;
 	else if (a == 7)
-		matrix[2][0] = player;
+		array[2][0] = player;
 	else if (a == 8)
-		matrix[2][1] = player;
+		array[2][1] = player;
 	else if (a == 9)
-		matrix[2][2] = player;
+		array[2][2] = player;
 }
 void TogglePlayer()
 {
@@ -56,43 +56,43 @@ void TogglePlayer()
 char Win()
 {
 	//first player
-	if (matrix[0][0] == 'X' && matrix[0][1] == 'X' && matrix[0][2] == 'X')
+	if (array[0][0] == 'X' && array[0][1] == 'X' && array[0][2] == 'X')
 		return 'X';
-	if (matrix[1][0] == 'X' && matrix[1][1] == 'X' && matrix[1][2] == 'X')
+	if (array[1][0] == 'X' && array[1][1] == 'X' && array[1][2] == 'X')
 		return 'X';
-	if (matrix[2][0] == 'X' && matrix[2][1] == 'X' && matrix[2][2] == 'X')
-		return 'X';
-
-	if (matrix[0][0] == 'X' && matrix[1][0] == 'X' && matrix[2][0] == 'X')
-		return 'X';
-	if (matrix[0][1] == 'X' && matrix[1][1] == 'X' && matrix[2][1] == 'X')
-		return 'X';
-	if (matrix[0][2] == 'X' && matrix[1][2] == 'X' && matrix[2][2] == 'X')
+	if (array[2][0] == 'X' && array[2][1] == 'X' && array[2][2] == 'X')
 		return 'X';
 
-	if (matrix[0][0] == 'X' && matrix[1][1] == 'X' && matrix[2][2] == 'X')
+	if (array[0][0] == 'X' && array[1][0] == 'X' && array[2][0] == 'X')
 		return 'X';
-	if (matrix[2][0] == 'X' && matrix[1][1] == 'X' && matrix[0][2] == 'X')
+	if (array[0][1] == 'X' && array[1][1] == 'X' && array[2][1] == 'X')
+		return 'X';
+	if (array[0][2] == 'X' && array[1][2] == 'X' && array[2][2] == 'X')
+		return 'X';
+
+	if (array[0][0] == 'X' && array[1][1] == 'X' && array[2][2] == 'X')
+		return 'X';
+	if (array[2][0] == 'X' && array[1][1] == 'X' && array[0][2] == 'X')
 		return 'X';
 
 	//second player
-	if (matrix[0][0] == 'O' && matrix[0][1] == 'O' && matrix[0][2] == 'O')
+	if (array[0][0] == 'O' && array[0][1] == 'O' && array[0][2] == 'O')
 		return 'O';
-	if (matrix[1][0] == 'O' && matrix[1][1] == 'O' && matrix[1][2] == 'O')
+	if (array[1][0] == 'O' && array[1][1] == 'O' && array[1][2] == 'O')
 		return 'O';
-	if (matrix[2][0] == 'O' && matrix[2][1] == 'O' && matrix[2][2] == 'O')
-		return 'O';
-
-	if (matrix[0][0] == 'O' && matrix[1][0] == 'O' && matrix[2][0] == 'O')
-		return 'O';
-	if (matrix[0][1] == 'O' && matrix[1][1] == 'O' && matrix[2][1] == 'O')
-		return 'O';
-	if (matrix[0][2] == 'O' && matrix[1][2] == 'O' && matrix[2][2] == 'O')
+	if (array[2][0] == 'O' && array[2][1] == 'O' && array[2][2] == 'O')
 		return 'O';
 
-	if (matrix[0][0] == 'O' && matrix[1][1] == 'O' && matrix[2][2] == 'O')
+	if (array[0][0] == 'O' && array[1][0] == 'O' && array[2][0] == 'O')
 		return 'O';
-	if (matrix[2][0] == 'O' && matrix[1][1] == 'O' && matrix[0][2] == 'O')
+	if (array[0][1] == 'O' && array[1][1] == 'O' && array[2][1] == 'O')
+		return 'O';
+	if (array[0][2] == 'O' && array[1][2] == 'O' && array[2][2] == 'O')
+		return 'O';
+
+	if (array[0][0] == 'O' && array[1][1] == 'O' && array[2][2] == 'O')
+		return 'O';
+	if (array[2][0] == 'O' && array[1][1] == 'O' && array[0][2] == 'O')
 		return 'O';
 
 	return '/';
