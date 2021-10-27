c717c4b884d5c55d110bd7d46895add7ed5e8bce
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2021-10-27 16:50:05 -0700

    Update tic.cpp

--- tic.cpp
+++ tic.cpp
@@ -5,109 +5,116 @@
 
 using namespace std;
 
-/*void printBoard(char * array, int r, int c);
-void reset(char array[3][3]);*/
-
-void printPointer(char** arrPtrParam);
-
-int main(){
-  
-  char** arrPtr = new char*[3];
-  for(int i = 0; i < 3; i++){
-    arrPtr[i] = new char[3];
-    for(int j = 0; j < 3; j++){
-      arrPtr[i][j] = 'a';
-    }
-  }
-  printPointer(arrPtr);
-  
-  /*char array[3][3] = {
-
-    {'_','_','_'},
-    {'_','_','_'},
-    {'_','_','_'}
-
-  };*/
-  
-  
-  
- /*cout << "Welcome to Tic Tac Toe. Player one will be X, and Player 2 will by O" << endl;
- cout << "You will enter where to place your letter based on the following format" << endl;
- cout << "Row 1 = 1, Row 2 = 2, Row 3 = 3" << endl;
- cout << "Col 1 = a, Col 2 = b, Col 3 = c" << endl;
- cout << "Please enter which row you want to place your move (1,2,3)" << endl;
- char x = 0;
- char y = 0;
- cin >> x;
- cout << "Please enter which column you want to place your move (a,b,c)" << endl;
- cin >> y;
- array[x-1][y-1] = 'x';*/
- 
-  
-};
-
-void printPointer(char** arrPtrParam) {
-for (int a = 0; a < 3; a++) {
-	    for (int b = 0; b < 3; b++) {
-	      arrPtrParam[a][b] += 100;
-	      cout << arrPtrParam[a][b] << " ";
-	   }
-	    cout << endl;
-	 }
+char matrix[3][3] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+char player = 'X';
+void Draw()
+{
+	system("cls");
+	cout << "Tic Tac Toe v1.0" << endl;
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cout << matrix[i][j] << " ";
+		}
+		cout << endl;
+	}
 }
+void Input()
+{
+	int a;
+	cout << "Press the number of the field: ";
+	cin >> a;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*void playX(char array[3][3]){  
- cout << "Please enter which row you want to place your move (1,2,3)" << endl;
- char x = 0;
- char y = 0;
- cin >> x;
- cout << "Please enter which column you want to place your move (a,b,c)" << endl;
- cin >> y;
- array[x-1][y-1] = 'x';
+	if (a == 1)
+		matrix[0][0] = player;
+	else if (a == 2)
+		matrix[0][1] = player;
+	else if (a == 3)
+		matrix[0][2] = player;
+	else if (a == 4)
+		matrix[1][0] = player;
+	else if (a == 5)
+		matrix[1][1] = player;
+	else if (a == 6)
+		matrix[1][2] = player;
+	else if (a == 7)
+		matrix[2][0] = player;
+	else if (a == 8)
+		matrix[2][1] = player;
+	else if (a == 9)
+		matrix[2][2] = player;
 }
-
-void playO(char array[3][3]){  
- cout << "Please enter which row you want to place your move (1,2,3)" << endl;
- char x = 0;
- char y = 0;
- cin >> x;
- cout << "Please enter which column you want to place your move (a,b,c)" << endl;
- cin >> y;
- array[x-1][y-1] = 'o';
+void TogglePlayer()
+{
+	if (player == 'X')
+		player = 'O';
+	else
+		player = 'X';
 }
+char Win()
+{
+	//first player
+	if (matrix[0][0] == 'X' && matrix[0][1] == 'X' && matrix[0][2] == 'X')
+		return 'X';
+	if (matrix[1][0] == 'X' && matrix[1][1] == 'X' && matrix[1][2] == 'X')
+		return 'X';
+	if (matrix[2][0] == 'X' && matrix[2][1] == 'X' && matrix[2][2] == 'X')
+		return 'X';
 
-void reset(char array[3][3]){
-  for(int i = 0; i < 3; i++){
-    for(int j = 0; j < 3; j++){
-        array[i][j] = '_';
-    }
-  }
-  //printBoard(array);
-  return;
+	if (matrix[0][0] == 'X' && matrix[1][0] == 'X' && matrix[2][0] == 'X')
+		return 'X';
+	if (matrix[0][1] == 'X' && matrix[1][1] == 'X' && matrix[2][1] == 'X')
+		return 'X';
+	if (matrix[0][2] == 'X' && matrix[1][2] == 'X' && matrix[2][2] == 'X')
+		return 'X';
+
+	if (matrix[0][0] == 'X' && matrix[1][1] == 'X' && matrix[2][2] == 'X')
+		return 'X';
+	if (matrix[2][0] == 'X' && matrix[1][1] == 'X' && matrix[0][2] == 'X')
+		return 'X';
+
+	//second player
+	if (matrix[0][0] == 'O' && matrix[0][1] == 'O' && matrix[0][2] == 'O')
+		return 'O';
+	if (matrix[1][0] == 'O' && matrix[1][1] == 'O' && matrix[1][2] == 'O')
+		return 'O';
+	if (matrix[2][0] == 'O' && matrix[2][1] == 'O' && matrix[2][2] == 'O')
+		return 'O';
+
+	if (matrix[0][0] == 'O' && matrix[1][0] == 'O' && matrix[2][0] == 'O')
+		return 'O';
+	if (matrix[0][1] == 'O' && matrix[1][1] == 'O' && matrix[2][1] == 'O')
+		return 'O';
+	if (matrix[0][2] == 'O' && matrix[1][2] == 'O' && matrix[2][2] == 'O')
+		return 'O';
+
+	if (matrix[0][0] == 'O' && matrix[1][1] == 'O' && matrix[2][2] == 'O')
+		return 'O';
+	if (matrix[2][0] == 'O' && matrix[1][1] == 'O' && matrix[0][2] == 'O')
+		return 'O';
+
+	return '/';
 }
-
-void printBoard(char * array, int r, int c){
-  for(int i = 0; i < r; i++){
-    for(int j = 0; j < c; j++){
-      cout << *(array+i*c+i);
-    }
-    cout << endl;
-  }
-  cout << "-----------------------" << endl;
-  return;
-} */
-
+int main()
+{
+	Draw();
+	while (1)
+	{
+		Input();
+		Draw();
+		if (Win() == 'X')
+		{
+			cout << "X wins!" << endl;
+			break;
+		}
+		else if (Win() == 'O')
+		{
+			cout << "O wins!" << endl;
+			break;
+		}
+		TogglePlayer();
+	}
+	system("pause");
+	return 0;
+}
