d311fd4ddcc223cbfd463573e193b3fd41ce02d7
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2021-10-27 17:10:10 -0700

    Update tic.cpp

--- tic.cpp
+++ tic.cpp
@@ -5,7 +5,11 @@
 
 using namespace std;
 
-char board[3][3] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+char board[3][3] = { 
+	{'1', '2', '3'}, 
+	{'4', '5', '6'},
+	{'7', '8', '9' }
+};
 char player = 'X';
 void Draw();
 void Input();
