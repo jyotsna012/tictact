9fc53e8753b8dd8aec70581675fd7148400fd1d3
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2021-10-27 17:04:15 -0700

    Update tic.cpp

--- tic.cpp
+++ tic.cpp
@@ -5,17 +5,15 @@
 
 using namespace std;
 
-char matrix[3][3] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+char board[3][3] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 char player = 'X';
 void Draw()
 {
-	//system("cls");
-	cout << "Tic Tac Toe v1.0" << endl;
 	for (int i = 0; i < 3; i++)
 	{
 		for (int j = 0; j < 3; j++)
 		{
-			cout << matrix[i][j] << " ";
+			cout << board[i][j] << " ";
 		}
 		cout << endl;
 	}
@@ -27,23 +25,23 @@ void Input()
 	cin >> a;
 
 	if (a == 1)
-		matrix[0][0] = player;
+		board[0][0] = player;
 	else if (a == 2)
-		matrix[0][1] = player;
+		board[0][1] = player;
 	else if (a == 3)
-		matrix[0][2] = player;
+		board[0][2] = player;
 	else if (a == 4)
-		matrix[1][0] = player;
+		board[1][0] = player;
 	else if (a == 5)
-		matrix[1][1] = player;
+		board[1][1] = player;
 	else if (a == 6)
-		matrix[1][2] = player;
+		board[1][2] = player;
 	else if (a == 7)
-		matrix[2][0] = player;
+		board[2][0] = player;
 	else if (a == 8)
-		matrix[2][1] = player;
+		board[2][1] = player;
 	else if (a == 9)
-		matrix[2][2] = player;
+		board[2][2] = player;
 }
 void TogglePlayer()
 {
@@ -55,43 +53,43 @@ void TogglePlayer()
 char Win()
 {
 	//first player
-	if (matrix[0][0] == 'X' && matrix[0][1] == 'X' && matrix[0][2] == 'X')
+	if (board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X')
 		return 'X';
-	if (matrix[1][0] == 'X' && matrix[1][1] == 'X' && matrix[1][2] == 'X')
+	if (board[1][0] == 'X' && board[1][1] == 'X' && board[1][2] == 'X')
 		return 'X';
-	if (matrix[2][0] == 'X' && matrix[2][1] == 'X' && matrix[2][2] == 'X')
-		return 'X';
-
-	if (matrix[0][0] == 'X' && matrix[1][0] == 'X' && matrix[2][0] == 'X')
-		return 'X';
-	if (matrix[0][1] == 'X' && matrix[1][1] == 'X' && matrix[2][1] == 'X')
-		return 'X';
-	if (matrix[0][2] == 'X' && matrix[1][2] == 'X' && matrix[2][2] == 'X')
+	if (board[2][0] == 'X' && board[2][1] == 'X' && board[2][2] == 'X')
 		return 'X';
 
-	if (matrix[0][0] == 'X' && matrix[1][1] == 'X' && matrix[2][2] == 'X')
+	if (board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] == 'X')
 		return 'X';
-	if (matrix[2][0] == 'X' && matrix[1][1] == 'X' && matrix[0][2] == 'X')
+	if (board[0][1] == 'X' && board[1][1] == 'X' && board[2][1] == 'X')
+		return 'X';
+	if (board[0][2] == 'X' && board[1][2] == 'X' && board[2][2] == 'X')
+		return 'X';
+
+	if (board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X')
+		return 'X';
+	if (board[2][0] == 'X' && board[1][1] == 'X' && board[0][2] == 'X')
 		return 'X';
 
 	//second player
-	if (matrix[0][0] == 'O' && matrix[0][1] == 'O' && matrix[0][2] == 'O')
+	if (board[0][0] == 'O' && board[0][1] == 'O' && board[0][2] == 'O')
 		return 'O';
-	if (matrix[1][0] == 'O' && matrix[1][1] == 'O' && matrix[1][2] == 'O')
+	if (board[1][0] == 'O' && board[1][1] == 'O' && board[1][2] == 'O')
 		return 'O';
-	if (matrix[2][0] == 'O' && matrix[2][1] == 'O' && matrix[2][2] == 'O')
-		return 'O';
-
-	if (matrix[0][0] == 'O' && matrix[1][0] == 'O' && matrix[2][0] == 'O')
-		return 'O';
-	if (matrix[0][1] == 'O' && matrix[1][1] == 'O' && matrix[2][1] == 'O')
-		return 'O';
-	if (matrix[0][2] == 'O' && matrix[1][2] == 'O' && matrix[2][2] == 'O')
+	if (board[2][0] == 'O' && board[2][1] == 'O' && board[2][2] == 'O')
 		return 'O';
 
-	if (matrix[0][0] == 'O' && matrix[1][1] == 'O' && matrix[2][2] == 'O')
+	if (board[0][0] == 'O' && board[1][0] == 'O' && board[2][0] == 'O')
 		return 'O';
-	if (matrix[2][0] == 'O' && matrix[1][1] == 'O' && matrix[0][2] == 'O')
+	if (board[0][1] == 'O' && board[1][1] == 'O' && board[2][1] == 'O')
+		return 'O';
+	if (board[0][2] == 'O' && board[1][2] == 'O' && board[2][2] == 'O')
+		return 'O';
+
+	if (board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O')
+		return 'O';
+	if (board[2][0] == 'O' && board[1][1] == 'O' && board[0][2] == 'O')
 		return 'O';
 
 	return '/';
@@ -115,6 +113,7 @@ int main()
 		}
 		TogglePlayer();
 	}
-	system("pause");
 	return 0;
 }
+
+
