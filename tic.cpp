d7cd1419af6cc7eec1e5292077fd606db9071adb
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2021-11-03 22:10:59 -0700

    Update tic.cpp

--- tic.cpp
+++ tic.cpp
@@ -218,45 +218,43 @@ void TogglePlayer()
 }
 char Win()
 {
-	//first player
-	if (board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X')
-		return 'X';
-	if (board[1][0] == 'X' && board[1][1] == 'X' && board[1][2] == 'X')
-		return 'X';
-	if (board[2][0] == 'X' && board[2][1] == 'X' && board[2][2] == 'X')
-		return 'X';
+	if (board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X'){
+		return 'X';}
+	if (board[1][0] == 'X' && board[1][1] == 'X' && board[1][2] == 'X'){
+		return 'X';}
+	if (board[2][0] == 'X' && board[2][1] == 'X' && board[2][2] == 'X'){
+		return 'X';}
 
-	if (board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] == 'X')
-		return 'X';
-	if (board[0][1] == 'X' && board[1][1] == 'X' && board[2][1] == 'X')
-		return 'X';
-	if (board[0][2] == 'X' && board[1][2] == 'X' && board[2][2] == 'X')
-		return 'X';
+	if (board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] == 'X'){
+		return 'X';}
+	if (board[0][1] == 'X' && board[1][1] == 'X' && board[2][1] == 'X'){
+		return 'X';}
+	if (board[0][2] == 'X' && board[1][2] == 'X' && board[2][2] == 'X'){
+		return 'X';}
 
-	if (board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X')
-		return 'X';
-	if (board[2][0] == 'X' && board[1][1] == 'X' && board[0][2] == 'X')
-		return 'X';
+	if (board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X'){
+		return 'X';}
+	if (board[2][0] == 'X' && board[1][1] == 'X' && board[0][2] == 'X'){
+		return 'X';}
 
-	//second player
-	if (board[0][0] == 'O' && board[0][1] == 'O' && board[0][2] == 'O')
-		return 'O';
-	if (board[1][0] == 'O' && board[1][1] == 'O' && board[1][2] == 'O')
-		return 'O';
-	if (board[2][0] == 'O' && board[2][1] == 'O' && board[2][2] == 'O')
-		return 'O';
+	if (board[0][0] == 'O' && board[0][1] == 'O' && board[0][2] == 'O'){
+		return 'O';}
+	if (board[1][0] == 'O' && board[1][1] == 'O' && board[1][2] == 'O'){
+		return 'O';}
+	if (board[2][0] == 'O' && board[2][1] == 'O' && board[2][2] == 'O'){
+		return 'O';}
 
-	if (board[0][0] == 'O' && board[1][0] == 'O' && board[2][0] == 'O')
-		return 'O';
-	if (board[0][1] == 'O' && board[1][1] == 'O' && board[2][1] == 'O')
-		return 'O';
-	if (board[0][2] == 'O' && board[1][2] == 'O' && board[2][2] == 'O')
-		return 'O';
+	if (board[0][0] == 'O' && board[1][0] == 'O' && board[2][0] == 'O'){
+		return 'O';}
+	if (board[0][1] == 'O' && board[1][1] == 'O' && board[2][1] == 'O'){
+		return 'O';}
+	if (board[0][2] == 'O' && board[1][2] == 'O' && board[2][2] == 'O'){
+		return 'O';}
 
-	if (board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O')
-		return 'O';
-	if (board[2][0] == 'O' && board[1][1] == 'O' && board[0][2] == 'O')
-		return 'O';
+	if (board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O'){
+		return 'O';}
+	if (board[2][0] == 'O' && board[1][1] == 'O' && board[0][2] == 'O'){
+		return 'O';}
 
 	return 'n';
 }
